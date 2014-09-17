e508e7f3480805d32b4db4f26b853cf16d0f15a8
Caio SBA <caiosba@gmail.com>
2014-09-17 11:45:34 -0300

    Code is compiling now

---
+++ apps/CMakeFiles/demo.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/caiosba/Mestrado/kinfu_remake/apps/demo.cpp" "/home/caiosba/Mestrado/kinfu_remake/apps/CMakeFiles/demo.dir/demo.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/caiosba/Mestrado/kinfu_remake/kfusion/CMakeFiles/kfusion.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "/usr/local/cuda-5.5/include"
+  "/usr/include/openni"
+  "kfusion/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ kfusion/CMakeFiles/kfusion.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/caiosba/Mestrado/kinfu_remake/kfusion/src/capture.cpp" "/home/caiosba/Mestrado/kinfu_remake/kfusion/CMakeFiles/kfusion.dir/src/capture.cpp.o"
+  "/home/caiosba/Mestrado/kinfu_remake/kfusion/src/core.cpp" "/home/caiosba/Mestrado/kinfu_remake/kfusion/CMakeFiles/kfusion.dir/src/core.cpp.o"
+  "/home/caiosba/Mestrado/kinfu_remake/kfusion/src/device_memory.cpp" "/home/caiosba/Mestrado/kinfu_remake/kfusion/CMakeFiles/kfusion.dir/src/device_memory.cpp.o"
+  "/home/caiosba/Mestrado/kinfu_remake/kfusion/src/imgproc.cpp" "/home/caiosba/Mestrado/kinfu_remake/kfusion/CMakeFiles/kfusion.dir/src/imgproc.cpp.o"
+  "/home/caiosba/Mestrado/kinfu_remake/kfusion/src/kinfu.cpp" "/home/caiosba/Mestrado/kinfu_remake/kfusion/CMakeFiles/kfusion.dir/src/kinfu.cpp.o"
+  "/home/caiosba/Mestrado/kinfu_remake/kfusion/src/precomp.cpp" "/home/caiosba/Mestrado/kinfu_remake/kfusion/CMakeFiles/kfusion.dir/src/precomp.cpp.o"
+  "/home/caiosba/Mestrado/kinfu_remake/kfusion/src/projective_icp.cpp" "/home/caiosba/Mestrado/kinfu_remake/kfusion/CMakeFiles/kfusion.dir/src/projective_icp.cpp.o"
+  "/home/caiosba/Mestrado/kinfu_remake/kfusion/src/tsdf_volume.cpp" "/home/caiosba/Mestrado/kinfu_remake/kfusion/CMakeFiles/kfusion.dir/src/tsdf_volume.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "KFUSION_API_EXPORTS"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "/usr/local/cuda-5.5/include"
+  "/usr/include/openni"
+  "kfusion/include"
+  "kfusion/src"
+  "kfusion/src/cuda"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
